56a0ea89490694dc9d4bd452b7cc6743b13605c4
michelledeguzmandaluz <23-16852@g.batstate-u.edu.ph>
2024-10-10 10:01:04 +0800

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,42 +1,29 @@
-public class Dog{
-  string name;
-  boolean isMale;
-  int NoOfPuppies;
-  Dog[] listOfPuppies;
+class Dog {
+    String name;
+    boolean isMale;
+    int numPuppies;
+    Dog[] listPuppies;
 
-  public int addPuppies(Dog x){
-     listOfPuppies[NoOfPuppies] = x;
-     return NoOfPuppies;
-  }
-
-  public void SetName(String names){
-     name = names;
-  }
-  
-  public string ShowName(){
-    System.out.println("My name is "+ name);
-    return name;
-  }
-
-  public void NoOfPuppies(int x){
-    listOfPuppies = new Dog[x];
-  }
-
-  public void bark(){
-    System.out.println(name + " Woof! woof!");
-  }
-
-  public void ShowNoOfPuppies(){
-    System.out.println("Number of puppies " + NoOfPuppies);
-  
-  }
-
-  public void ShowAllPuppies(){
-    for(int i = 0; i < NoOfPuppies; ++i){
-      System.out.println(" " + "index:" + i);
-      System.out.println("Name:"+ listOfPuppies[i].name);
-      System.out.println("Is male:"+ listOfPuppies[i].name);
+    public Dog setName(String name) {
+        this.name = name;
+        return this;
     }
-  }
-
- }      
+    public String showName() {
+        return name;
+    }
+    public void setNumPuppies(int numPuppies) {
+        this.numPuppies = numPuppies;
+    }
+    public int showPuppies() {
+        return numPuppies;
+    }
+    public void setPuppies(Dog[] puppies) {
+        this.listPuppies = puppies;
+        this.numPuppies = puppies.length;
+    }
+    public void showPuppyNames() {
+        for (Dog puppy : listPuppies) {
+            System.out.println("Puppy name: " + puppy.showName());
+        }
+    }
+}
