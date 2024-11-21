5d9ecfd5b1ccd214910320887cacc201e3909f69
michelledeguzmandaluz <23-16852@g.batstate-u.edu.ph>
2024-11-22 07:22:25 +0800

    Delete StudentClass.java

--- StudentClass.java
+++
@@ -1,7 +0,0 @@
-public class StudentClass{
-    public static void main(String[] args){
-        Student student1 = new Student();
-        student1.setId(1234);
-        student1.setname("Student_1");
-    }
-}
