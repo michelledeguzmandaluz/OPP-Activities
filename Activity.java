2bb659be513d9c51be2e06dcd3a3aa2a2ed5940c
michelledeguzmandaluz <23-16852@g.batstate-u.edu.ph>
2024-11-22 07:24:43 +0800

    Create Activity.java

---
+++ Activity.java
@@ -0,0 +1,18 @@
+public class Activity {
+    public static void main(String[] args) {
+        
+        Student student = new Student("Patrick", "Balayan", "BSIT", 2);
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Address: " + student.getAddress());
+        System.out.println("student Program: " + student.getProgram());
+        System.out.println("Student Year: " + student.getYear());
+        System.out.println(student.toString());
+        
+        Staff staff = new Staff("Russel", "San Piro", "BSU", 253000);
+        System.out.println("Staff Name: " + staff.getName());
+        System.out.println("Staff Address: " + staff.getAddress());
+        System.out.println("Staff School: " + staff.getSchool());
+        System.out.println("Staff Salary: " + staff.getSalary());
+        System.out.println(staff.toString());
+    }
+}
